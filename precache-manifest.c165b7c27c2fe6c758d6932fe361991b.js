83c646d2210466498a66fcfd37645c66070d4fab
Patryk <patrykkoski1994@gmail.com>
2019-05-18 22:27:26 +0200

    Updates

---
+++ precache-manifest.c165b7c27c2fe6c758d6932fe361991b.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783794da6b883fb9aa913395df856fa5",
+    "url": "/ice-cream/index.html"
+  },
+  {
+    "revision": "f860854a11353fbf7037",
+    "url": "/ice-cream/static/css/main.aa093778.chunk.css"
+  },
+  {
+    "revision": "71df6bc42be54b08fcff",
+    "url": "/ice-cream/static/js/2.bfe75898.chunk.js"
+  },
+  {
+    "revision": "f860854a11353fbf7037",
+    "url": "/ice-cream/static/js/main.70bfb047.chunk.js"
+  },
+  {
+    "revision": "4cd096138e0d214582fc",
+    "url": "/ice-cream/static/js/runtime~main.4fa864c1.js"
+  },
+  {
+    "revision": "5e3b8c3faf790b2f972db454b24960f3",
+    "url": "/ice-cream/static/media/Banan.5e3b8c3f.png"
+  },
+  {
+    "revision": "eb8232929ac643471869fc09c31c9a66",
+    "url": "/ice-cream/static/media/Bita Śmietana.eb823292.png"
+  },
+  {
+    "revision": "8991f4f778bb29fac470a76c439e03f7",
+    "url": "/ice-cream/static/media/Bonitki.8991f4f7.png"
+  },
+  {
+    "revision": "7c8bd8c89310c6af97153a70a9f1bc5a",
+    "url": "/ice-cream/static/media/Bounty.7c8bd8c8.png"
+  },
+  {
+    "revision": "e9523f6249c89a96f49efa810e40942b",
+    "url": "/ice-cream/static/media/Haribo Złote Misie.e9523f62.png"
+  },
+  {
+    "revision": "a2f74f7bd3f1bd8565a730989619371f",
+    "url": "/ice-cream/static/media/Jagoda.a2f74f7b.png"
+  },
+  {
+    "revision": "4373074e9ec17a6431bd6c96e26e9d16",
+    "url": "/ice-cream/static/media/Kinder Bueno.4373074e.png"
+  },
+  {
+    "revision": "87f40de29c49b146d6a1e0b1dc20fa5f",
+    "url": "/ice-cream/static/media/Kinder Maxi King.87f40de2.png"
+  },
+  {
+    "revision": "80d70b8bf807d00a37a1fcc1fd9fda2f",
+    "url": "/ice-cream/static/media/Kinder Pingui.80d70b8b.png"
+  },
+  {
+    "revision": "2631ae720f12c717154fdca3fa6d1e81",
+    "url": "/ice-cream/static/media/M&M's.2631ae72.png"
+  },
+  {
+    "revision": "b1976dd7238d9f1e6f1fbcedd0b585f6",
+    "url": "/ice-cream/static/media/Malina.b1976dd7.png"
+  },
+  {
+    "revision": "f5886b1810304018b1bfffeeb74329e6",
+    "url": "/ice-cream/static/media/Michałki z Hanki Milk.f5886b18.png"
+  },
+  {
+    "revision": "78fd9bc2b5b653f93cd44fb72585bf49",
+    "url": "/ice-cream/static/media/Michałki z Hanki.78fd9bc2.png"
+  },
+  {
+    "revision": "3885db752f3024239879fd618cf52845",
+    "url": "/ice-cream/static/media/Nescafe.3885db75.png"
+  },
+  {
+    "revision": "fe9d9d094c2b380de977850fb95e1831",
+    "url": "/ice-cream/static/media/Nutella.fe9d9d09.png"
+  },
+  {
+    "revision": "26c109a3e0204af5f10ca18231d8c06e",
+    "url": "/ice-cream/static/media/Oreo.26c109a3.png"
+  },
+  {
+    "revision": "86972a7ec7fb9bf0287563c609691385",
+    "url": "/ice-cream/static/media/Pastylki Miętowe.86972a7e.png"
+  },
+  {
+    "revision": "db1b55bc36fe13f1ef7bbf591e90d56f",
+    "url": "/ice-cream/static/media/Pomarańcza.db1b55bc.png"
+  },
+  {
+    "revision": "fc4a0531b80512fca56e9bdc3f9d565f",
+    "url": "/ice-cream/static/media/Princessa.fc4a0531.png"
+  },
+  {
+    "revision": "6edd4d05016dc0ae416ec652800d2c13",
+    "url": "/ice-cream/static/media/Rodzynki.6edd4d05.png"
+  },
+  {
+    "revision": "899274cfb0587eda18fed94f2645e95d",
+    "url": "/ice-cream/static/media/Toffi.899274cf.png"
+  },
+  {
+    "revision": "309532185d4224cb078381e6c16630f8",
+    "url": "/ice-cream/static/media/Truskawka.30953218.png"
+  }
+]);
